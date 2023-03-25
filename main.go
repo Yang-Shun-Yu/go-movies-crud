10d7fc9709db9adc21fb0ff79abad5ed8afb9f93
Yang-Shun-Yu <aebandafg@gmail.com>
2023-03-26 03:15:08 +0800

    0326

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
-	"mux-master"
+	// "math/rand"
+	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
 )
@@ -57,9 +57,29 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movie.ID = strconv.Itoa(len(movies)+1)
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	//set json content type
+	w.Header().Set("Content-Type", "application/json")
+	//params
+	params :=mux.Vars(r)
+	//loop over the movies,range
+	for index,item:=range movies{
+		if item.ID==params["id"]{
+			movies = append(movies[:index],movies[index+1:]...)
+			var movie Movie
+			_=json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+
+	}
 }
 func main() {
 	r := mux.NewRouter()
